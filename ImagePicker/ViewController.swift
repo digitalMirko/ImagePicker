63e7fbb58da4ac06f0a72377bccfe3bea5068581
Mirko Cukich <mirkocukich@gmail.com>
2016-12-18 00:07:06 -0600

    imagePickerController added

--- ImagePicker/ViewController.swift
+++ ImagePicker/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet var imageView: UIImageView!
     override func viewDidLoad() {
@@ -18,16 +18,22 @@ class ViewController: UIViewController {
 
     @IBAction func chooseImage(_ sender: Any) {
         
+        
+        // imagePickerController
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
         // give user choice - camera or photolibrary
         // pops up from bottom
         let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a source", preferredStyle: .actionSheet)
         // Camera Btn option
         actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action:UIAlertAction) in
-            
+            imagePickerController.sourceType = .camera
+            self.present(imagePickerController,animated: true, completion: nil)
         }))
         // Photo Library Btn option
         actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { (action:UIAlertAction) in
-
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController,animated: true, completion: nil)
         }))
         // Cancel Btn option
         actionSheet.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
@@ -40,6 +46,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        imageView.image = image
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    
 
 }
 
