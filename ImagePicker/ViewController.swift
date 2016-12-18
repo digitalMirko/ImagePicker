eb414cd4e81448fb5f6c3b6f18be83d333115b85
Mirko Cukich <mirkocukich@gmail.com>
2016-12-17 23:49:13 -0600

    Initial Commit

--- ImagePicker/ViewController.swift
+++ ImagePicker/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func chooseImage(_ sender: Any) {
+        
+        // give user choice - camera or photolibrary
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
